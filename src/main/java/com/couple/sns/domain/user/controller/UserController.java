7d259bc4434a5a81acdb2f95d07fea5b90dece9f
tudy <jhh0207@hanmail.net>
2023-01-27 21:56:07 +0900

    feat : 필요없는 import remove

--- src/main/java/com/couple/sns/domain/user/controller/UserController.java
+++ src/main/java/com/couple/sns/domain/user/controller/UserController.java
@@ -6,7 +6,6 @@ import com.couple.sns.domain.user.dto.request.UserJoinRequest;
 import com.couple.sns.domain.user.dto.response.UserJoinResponse;
 import com.couple.sns.domain.user.service.UserUpdateService;
 import lombok.RequiredArgsConstructor;
-import lombok.extern.slf4j.Slf4j;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
