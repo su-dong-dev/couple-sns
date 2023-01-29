26225660e891e93d3f82f26e1950a5860b7cd0d6
tudy <jhh0207@hanmail.net>
2023-01-29 23:12:53 +0900

    chore : jwt, configurationProperties 의존성 추가 #6

--- src/main/java/com/couple/sns/SnsApplication.java
+++ src/main/java/com/couple/sns/SnsApplication.java
@@ -3,7 +3,9 @@ package com.couple.sns;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.boot.autoconfigure.jdbc.DataSourceAutoConfiguration;
+import org.springframework.boot.context.properties.EnableConfigurationProperties;
 
+@EnableConfigurationProperties
 @SpringBootApplication
 public class SnsApplication {
 
