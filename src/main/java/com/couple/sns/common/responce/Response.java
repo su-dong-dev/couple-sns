5b90ca4f057c45487ba0c6760d66a8dd75bb607d
tudy <jhh0207@hanmail.net>
2023-03-11 23:39:10 +0900

    feat : #11 - response success 반환 메드 추가
    이슈와 관련 없지만 해당 이슈에 대한 코드 구현을 위해 필요

--- src/main/java/com/couple/sns/common/responce/Response.java
+++ src/main/java/com/couple/sns/common/responce/Response.java
@@ -14,6 +14,10 @@ public class Response<T> {
         return new Response<>(errorCode, null);
     }
 
+    public static Response<Void> success() {
+        return new Response<>("SUCCESS", null);
+    }
+
     public static <T> Response<T> success(T result) {
         return new Response<>("SUCCESS", result);
     }
