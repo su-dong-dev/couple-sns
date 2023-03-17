2cd6207358965e668c49a31c0b0e3d6834282021
tudy <jhh0207@hanmail.net>
2023-03-17 17:52:44 +0900

    feat : #11 - post 작성/수정 api test code 구현 & 테스트 Entity 생성

--- src/test/java/com/couple/sns/domain/post/controller/PostControllerTest.java
+++ src/test/java/com/couple/sns/domain/post/controller/PostControllerTest.java
@@ -1,19 +1,29 @@
 package com.couple.sns.domain.post.controller;
 
+import static org.mockito.ArgumentMatchers.any;
+import static org.mockito.ArgumentMatchers.eq;
+import static org.mockito.BDDMockito.given;
 import static org.mockito.BDDMockito.willDoNothing;
 import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.post;
+import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.put;
 import static org.springframework.test.web.servlet.result.MockMvcResultHandlers.print;
 import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.status;
 
+import com.couple.sns.common.exception.SnsApplicationException;
+import com.couple.sns.domain.common.fixture.PostEntityFixture;
+import com.couple.sns.domain.post.dto.Post;
 import com.couple.sns.domain.post.dto.request.PostCreateRequest;
+import com.couple.sns.domain.post.dto.request.PostModifyRequest;
 import com.couple.sns.domain.post.service.PostService;
 import com.fasterxml.jackson.databind.ObjectMapper;
 import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.autoconfigure.web.servlet.AutoConfigureMockMvc;
+import org.springframework.boot.test.autoconfigure.web.servlet.WebMvcTest;
 import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.boot.test.mock.mockito.MockBean;
 import org.springframework.http.MediaType;
+import org.springframework.security.test.context.support.WithAnonymousUser;
 import org.springframework.security.test.context.support.WithMockUser;
 import org.springframework.test.web.servlet.MockMvc;
 
@@ -28,12 +38,13 @@ public class PostControllerTest {
     private PostService postService;
 
     @Test
-    @WithMockUser()
+    @WithMockUser
     public void 포스트_작성_성공() throws Exception {
         String title = "title";
         String body = "body";
 
-        willDoNothing().given(postService).create(title,body,"userId");
+        given(postService.create(eq(title), eq(body), any()))
+            .willReturn(Post.fromEntity(PostEntityFixture.get("userId", 1L, 1L)));
 
         mockMvc.perform(post("/api/v1/posts")
                 .contentType(MediaType.APPLICATION_JSON)
@@ -41,4 +52,21 @@ public class PostControllerTest {
             ).andDo(print())
             .andExpect(status().isOk());
     }
+
+    @Test
+    @WithMockUser
+    public void 포스트_수정_성공() throws Exception {
+        String title = "modify_title";
+        String body = "modify_body";
+
+        given(postService.modify(any(),any(), any(), any()))
+            .willReturn(Post.fromEntity(PostEntityFixture.get("userId", 1L, 1L)));
+
+        mockMvc.perform(put("/api/v1/posts/1")
+                .contentType(MediaType.APPLICATION_JSON)
+                .content(objectMapper.writeValueAsBytes(new PostModifyRequest(title, body)))
+            ).andDo(print())
+            .andExpect(status().isOk());
+    }
+
 }
