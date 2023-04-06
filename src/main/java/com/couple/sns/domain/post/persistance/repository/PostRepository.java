91a0defd3132acee486af699dbdbb1900bddea09
tudy <jhh0207@hanmail.net>
2023-04-06 23:41:07 +0900

    feat : #15 - @ManyToOne N+1 이슈 관련 소스코드 수정

--- src/main/java/com/couple/sns/domain/post/persistance/repository/PostRepository.java
+++ src/main/java/com/couple/sns/domain/post/persistance/repository/PostRepository.java
@@ -4,12 +4,14 @@ import com.couple.sns.domain.post.persistance.PostEntity;
 import com.couple.sns.domain.user.persistance.UserEntity;
 import org.springframework.data.domain.Page;
 import org.springframework.data.domain.Pageable;
+import org.springframework.data.jpa.repository.EntityGraph;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
 
 @Repository
 public interface PostRepository extends JpaRepository<PostEntity, Long> {
 
+    @EntityGraph(attributePaths = {"user"})
     Page<PostEntity> findAllByUser(UserEntity userEntity, Pageable pageable);
 
 }
