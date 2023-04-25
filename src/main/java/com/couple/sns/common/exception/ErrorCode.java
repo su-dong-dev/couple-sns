8b6ae8e5f0ba165332fe2ec9c9bed34c7f91a623
tudy <jhh0207@hanmail.net>
2023-04-25 21:21:53 +0900

    feat : #22 - aws s3 에 여러 이미지 업로드 기능 구현

--- src/main/java/com/couple/sns/common/exception/ErrorCode.java
+++ src/main/java/com/couple/sns/common/exception/ErrorCode.java
@@ -15,6 +15,7 @@ public enum ErrorCode {
     POST_NOT_FOUND(HttpStatus.NOT_FOUND, "Post not founded"),
     INVALID_PERMISSION(HttpStatus.UNAUTHORIZED, "Permission is invalid"),
     ALREADY_LIKED_POST(HttpStatus.CONFLICT, "User already liked the post"),
+    BAD_REQUEST(HttpStatus.BAD_REQUEST, "Request is bad"),
 
     INTERNAL_SERVER_ERROR(HttpStatus.INTERNAL_SERVER_ERROR, "Internal server error"),
     ;
