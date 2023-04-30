f7458ad53598fdad2a09f73547af83ff30537e35
tudy <jhh0207@hanmail.net>
2023-04-30 14:36:17 +0900

    refactor : #24 - comment list 호출시 n+1 이슈 코드 수정

--- src/main/java/com/couple/sns/domain/post/persistance/repository/CommentRepository.java
+++ src/main/java/com/couple/sns/domain/post/persistance/repository/CommentRepository.java
@@ -3,13 +3,14 @@ package com.couple.sns.domain.post.persistance.repository;
 import com.couple.sns.domain.post.persistance.CommentEntity;
 import org.springframework.data.domain.Page;
 import org.springframework.data.domain.Pageable;
+import org.springframework.data.jpa.repository.EntityGraph;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
 
 @Repository
 public interface CommentRepository extends JpaRepository<CommentEntity, Long> {
 
+    @EntityGraph(attributePaths = {"user"})
     Page<CommentEntity> findByPostId(Long postId, Pageable pageable);
-
 
 }
